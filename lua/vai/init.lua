b01cf8be993fa09e4db77a34584f5ae4e3b1baad
John Mitsch <johnpmitsch@gmail.com>
2026-01-24 23:20:17 -0500

    Move require to inside function in init.lua

--- lua/vai/init.lua
+++ lua/vai/init.lua
@@ -1,14 +1,12 @@
 -- vai.nvim - Line jumping without numbers
 -- Jump to any visible line with 3 keystrokes: trigger + group + line
 
-local config = require("vai.config")
-local jump = require("vai.jump")
-
 local M = {}
 
 --- Setup vai.nvim with optional configuration
 ---@param opts? table Configuration options
 function M.setup(opts)
+	local config = require("vai.config")
 	config.setup(opts)
 
 	-- Create highlight groups
@@ -18,13 +16,13 @@ function M.setup(opts)
 	-- Set up keymaps
 	local trigger = config.options.trigger
 	vim.keymap.set({ "n", "x", "o" }, trigger, function()
-		jump.start()
+		require("vai.jump").start()
 	end, { desc = "vai: jump to line" })
 end
 
 --- Start a jump (can be called directly)
 function M.jump()
-	jump.start()
+	require("vai.jump").start()
 end
 
 return M
