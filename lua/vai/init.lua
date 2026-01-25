71ed85b951543b8c70686779192565993aea7d02
John Mitsch <johnpmitsch@gmail.com>
2026-01-24 23:24:46 -0500

    Revert "Move require to inside function in init.lua"
    This reverts commit b01cf8be993fa09e4db77a34584f5ae4e3b1baad.

--- lua/vai/init.lua
+++ lua/vai/init.lua
@@ -1,12 +1,14 @@
 -- vai.nvim - Line jumping without numbers
 -- Jump to any visible line with 3 keystrokes: trigger + group + line
 
+local config = require("vai.config")
+local jump = require("vai.jump")
+
 local M = {}
 
 --- Setup vai.nvim with optional configuration
 ---@param opts? table Configuration options
 function M.setup(opts)
-	local config = require("vai.config")
 	config.setup(opts)
 
 	-- Create highlight groups
@@ -16,13 +18,13 @@ function M.setup(opts)
 	-- Set up keymaps
 	local trigger = config.options.trigger
 	vim.keymap.set({ "n", "x", "o" }, trigger, function()
-		require("vai.jump").start()
+		jump.start()
 	end, { desc = "vai: jump to line" })
 end
 
 --- Start a jump (can be called directly)
 function M.jump()
-	require("vai.jump").start()
+	jump.start()
 end
 
 return M
